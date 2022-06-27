09ffec3efb6ccdd856ee031645539a88fe4a625a
Michael Thamm <84816828+MichaelThamm@users.noreply.github.com>
2022-06-27 11:37:29 -0400

    Update Main.py

--- Source/Main.py
+++ Source/Main.py
@@ -5,9 +5,8 @@ import configparser
 import requests
 import logging
 import json
-import os
-import re
 import copy
+import os
 
 
 PROJECT_PATH = Path(__file__).resolve().parent.parent
@@ -41,11 +40,6 @@ class ConnectAPI(object):
         LOGGER.log(logging.WARN, 'Implement run abstract method')
         return State.NOT_IMPLEMENTED
 
-    @abstractmethod
-    def formatData(self):
-        LOGGER.log(logging.WARN, 'Implement formatData abstract method')
-        return State.NOT_IMPLEMENTED
-
     def connect(self):
         response_API = requests.get(self.api_address)
         self.status_code = response_API.status_code
@@ -53,7 +47,6 @@ class ConnectAPI(object):
             LOGGER.log(logging.INFO, f'Status error on connect: {self.getStatusCode()}')
             return State.ERROR
         self.raw_data = json.loads(response_API.text)
-        self.formatData()
 
     def getStatusCode(self):
         return config.get("HttpStatusCodes", f'http.status.code.{self.status_code}')
@@ -75,31 +68,24 @@ class DecodeAPI1(ConnectAPI):
             return
         if self.transformData() == State.ERROR:
             return
-        print(f'The result to part 1:\n{list(self.getTransformedData().values())}')
-
-    def formatData(self):
-        # for _station in self.raw_data:
-        #     self.formattedData[_station["id"]] = _station
-        pass
+        print(f'The result to part 1:\n{self.getTransformedData()}')
 
     def transformData(self):
         for _station in self.raw_data:
-            for _key, _value in _station.items():
-                if self.passFilter(_station):
+            if self.passFilter(_station):
 
-                    copiedStation = copy.deepcopy(_station)
-                    # Copy the existing key-value pairs
-                    self.transformedData.append(copiedStation)  # TODO Maybe done need deepcopy
+                copiedStation = copy.deepcopy(_station)
+                # Copy the existing key-value pairs
+                self.transformedData.append(copiedStation)
 
-                    # Add the new key-value pairs
-                    index = len(self.transformedData) - 1
-                    self.transformedData[index]["active"] = copiedStation["status"] == "aktiv"
-                    self.transformedData[index]["free_ratio"] = copiedStation["free_boxes"] / copiedStation["boxes"]
-                    self.transformedData[index]["coordinates"] = [copiedStation["longitude"], copiedStation["latitude"]]
+                # Add the new key-value pairs
+                index = len(self.transformedData) - 1
+                self.transformedData[index]["active"] = copiedStation["status"] == "aktiv"
+                self.transformedData[index]["free_ratio"] = copiedStation["free_boxes"] / copiedStation["boxes"]
+                self.transformedData[index]["coordinates"] = [copiedStation["longitude"], copiedStation["latitude"]]
 
-                    # Reorder the list to match the expected structure
-                    # TODO Continue here
-                    self.transformedData[index] = [self.transformedData[_key][k] for k in self.transformedKeys]
+                # Reorder and trim the station to match the expected structure
+                self.transformedData[index] = {k: self.transformedData[index][k] for k in self.transformedKeys}
 
         self.sortData()
 
@@ -107,23 +93,20 @@ class DecodeAPI1(ConnectAPI):
         return station["free_bikes"] != 0
 
     def sortData(self):
-        secondarySorted = dict(sorted(self.transformedData.items(), key=lambda item: item[1]["name"]))
-        primarySorted = dict(sorted(secondarySorted.items(), key=lambda item: item[1]["free_bikes"], reverse=True))
+        secondarySorted = list(sorted(self.transformedData, key=lambda station: station["name"]))
+        primarySorted = list(sorted(secondarySorted, key=lambda station: station["free_bikes"], reverse=True))
         self.transformedData = primarySorted
 
     def getTransformedData(self):
         return self.transformedData
 
     def setAddress(self):
-        for _key, _station in self.transformedData.items():
+        for _station in self.transformedData:
             addressStr = 'https://api.i-mobility.at/routing/api/v1/nearby_address'
             addressStr += f'?latitude={_station["coordinates"][1]}&longitude={_station["coordinates"][0]}'
             apiAddressEndpoint = DecodeAPI2(addressStr)
             apiAddressEndpoint.run()
             _station["address"] = apiAddressEndpoint.getAddress()
-
-    def getAddressFromAPI(self, apiRawData):
-        return
 
 
 class DecodeAPI2(ConnectAPI):
@@ -135,20 +118,8 @@ class DecodeAPI2(ConnectAPI):
         if self.connect() == State.ERROR:
             return
 
-    def formatData(self):
-        try:
-            idKey = re.search('"name": "[^"]*', self.raw_data)
-            if idKey:
-                foundText = idKey.group(0)
-                address = foundText.replace('"name": "', '')
-                self.formattedData["address"] = address
-
-        except AttributeError:
-            LOGGER.log(logging.ERROR, f'The API data could not be formatted')
-            return State.ERROR
-
     def getAddress(self):
-        return self.formattedData["address"]
+        return self.raw_data["data"]["name"]
 
 
 if __name__ == "__main__":
@@ -158,5 +129,5 @@ if __name__ == "__main__":
 
     apiEndpoint1 = DecodeAPI1('https://wegfinder.at/api/v1/stations')
     apiEndpoint1.run()
-    # apiEndpoint1.setAddress()
-    print(f'The result to part 2:\n{list(apiEndpoint1.getTransformedData().values())}')
+    apiEndpoint1.setAddress()
+    print(f'The result to part 2:\n{list(apiEndpoint1.getTransformedData())}')
